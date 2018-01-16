5eecf3f11d65ba3ec6cfd0c728dccf4468db6432
tac <514162920@qq.com>
2018-01-16 17:50:12 +0800

    import project

---
+++ eureka/src/main/java/cn/tac/test/spring/cloud/eureka/EurekaApplication.java
@@ -0,0 +1,17 @@
+package cn.tac.test.spring.cloud.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author tac
+ * @since 16/01/2018
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}
