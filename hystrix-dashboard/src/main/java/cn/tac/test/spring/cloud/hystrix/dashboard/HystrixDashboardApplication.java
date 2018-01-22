4bda89142b0b7946cfd160347afee27a5c252608
tac <514162920@qq.com>
2018-01-22 14:49:00 +0800

    add hystrix dashboard

---
+++ hystrix-dashboard/src/main/java/cn/tac/test/spring/cloud/hystrix/dashboard/HystrixDashboardApplication.java
@@ -0,0 +1,17 @@
+package cn.tac.test.spring.cloud.hystrix.dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * @author tac
+ * @since 22/01/2018
+ */
+@SpringBootApplication
+@EnableHystrixDashboard
+public class HystrixDashboardApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboardApplication.class, args);
+    }
+}
